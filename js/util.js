ddff1172a4032d69998719ea0f2b3894d16e891b
Jupiter-Liar <jomaisaficusl@gmail.com>
2024-08-21 00:08:46 -0400

    alpha 7

--- js/util.js
+++ js/util.js
@@ -4,22 +4,22 @@ function dataBind(el, dataObject) {
 		return prop.charAt(0) === '!' ? !dataObject[prop.substr(1)] : dataObject[prop];
 	}
 
-    if (typeof el === 'string') {
+	if (typeof el === 'string') {
 		el = document.querySelector(el)
 	}
 	for (let tag of el.querySelectorAll('[data-bind]')) {
-			let prop = tag.getAttribute('data-bind');
+		let prop = tag.getAttribute('data-bind');
 		if (tag.tagName.toLowerCase() === 'input') {
 			if (tag.getAttribute('type').toLowerCase() === 'radio') {
 				tag.checked = dataObject[prop] === tag.getAttribute('value');
-            } else if (tag.getAttribute('type').toLowerCase() === 'checkbox') {
-                tag.checked = dataObject[prop];
-            } else {
-                tag.value = dataObject[prop];
+			} else if (tag.getAttribute('type').toLowerCase() === 'checkbox') {
+				tag.checked = dataObject[prop];
+			} else {
+				tag.value = dataObject[prop];
 			}
 		} else if (tag.tagName.toLowerCase() === 'select') {
 			for (let opt of tag.querySelectorAll('option')) {
-				if (opt.getAttribute('value') === dataObject[prop]) {
+				if (opt.getAttribute('value') === dataObject[prop]) {
 					opt.setAttribute('selected', 'selected');
 				} else {
 					opt.removeAttribute('selected');
@@ -50,14 +50,27 @@ function dataBind(el, dataObject) {
 			tag.removeAttribute('disabled');
 		}
 	}
+
+	var disableEl;
+
 	for (let tag of el.querySelectorAll('[data-class]')) {
 		let [className, prop] = tag.getAttribute('data-class').split(':');
 		let shouldHaveClass = boolValue(prop);
-		if (shouldHaveClass) {
+		disableEl = shouldHaveClass;
+
+		if (shouldHaveClass) {
 			tag.classList.add(className);
 		} else {
 			tag.classList.remove(className);
 		}
+	}
+
+	if (disableEl) {
+		el.classList.add('disabled');
+		el.classList.remove('enabled');
+	} else {
+		el.classList.remove('disabled');
+		el.classList.add('enabled');
 	}
 }
 
@@ -83,7 +96,7 @@ function showForm(selector, dataObject) {
 }
 
 function move(arr, from, to) {
-    arr.splice(to, 0, arr.splice(from, 1)[0]);
+	arr.splice(to, 0, arr.splice(from, 1)[0]);
 }
 
 function hideForm(selector) {
@@ -95,7 +108,9 @@ function hideForm(selector) {
 // Shows a message bar above the list of redirects.
 function showMessage(message, success) {
 	let messageBox = document.getElementById('message-box');
-	dataBind('#message-box', {message});
+	dataBind('#message-box', {
+		message
+	});
 	if (success) {
 		messageBox.className = 'visible success';
 	} else {
@@ -105,7 +120,7 @@ function showMessage(message, success) {
 	let timer = 20;
 
 	//Remove the message in 20 seconds if it hasn't been changed...
-	setTimeout(function() {
+	setTimeout(function () {
 		if (el('#message').textContent === message) {
 			messageBox.className = ''; //Removing .visible removes the box...
 		}
